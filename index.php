b880794cfd3018a1963d2c8025f9a28b6c6415bc
Itthiphol Eampoonga <42127273+itthiphol@users.noreply.github.com>
2022-07-08 15:23:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+     <?php echo '<p>My ID : 6500001</p>'; ?> 
+   <?php echo '<p>My name is Itthiphol</p>'; ?> 
+   <?php echo '<p>Update-1st</p>'; ?>  
+  
+ </body>
+</html>
